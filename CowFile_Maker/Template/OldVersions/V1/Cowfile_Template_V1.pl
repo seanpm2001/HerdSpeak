c7b68380ab53c213b8f5960ffff5d032db63ec4e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-05 16:13:31 -0700

    Cowfile_Template_V1.pl
    An archive of the first version of the Cowsay template.

---
+++ CowFile_Maker/Template/OldVersions/V1/Cowfile_Template_V1.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+# Start of script
+## /<Filename>.cow
+## <description_goes_here>
+## License: <license-spdx-identifier>
+## Author: <username> <email address> <other>
+## Start of cowfile
+$the_cow = <<EOC;
+  <ASCII_ART_GOES_HERE>
+  <$thoughts, $thoughts, $thoughts, $eyes, $udder, $tongue>
+  <$borg, $dead, $greedy, $paranoid, $stoned, $tired, $wired, $young>
+EOC
+# End of cowfile
+# File version: x (YYYYY/MM/DD @ 00:00)
+# Formatting: Perl 5
+# File type: Cowfile (*.cow)
+# Cowfile template version: 1 (2022, Tuesday, July 5th at 4:13 pm PST)
+# Backwards compatible with: Cowsay 2.x, Cowsay 3.x, HerdSpeak 1.x
+# End of script
